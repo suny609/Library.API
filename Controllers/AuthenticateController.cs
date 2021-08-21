e7f15abd851effa73d67bee7d90a4324d0d6ab93
勇 孙 <sy_609@hotmail.com>
2021-08-21 20:35:13 +0800

    0821

---
+++ Controllers/AuthenticateController.cs
@@ -0,0 +1,60 @@
+﻿using Library.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public AuthenticateController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpPost("token", Name = nameof(GenerateToken))]
+        public IActionResult GenerateToken(LoginUser loginUser)
+        {
+            if (loginUser.UserName != "demouser" || loginUser.Password != "demopassword")
+            {
+                return Unauthorized();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, loginUser.UserName)
+            };
+
+            var tokenConfigSection = Configuration.GetSection("Security:Token");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfigSection["Key"]));
+            var signCredential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var jwtToken = new JwtSecurityToken(
+                    issuer: tokenConfigSection["Issuer"],
+                    audience: tokenConfigSection["Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: signCredential
+                );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                expiration = TimeZoneInfo.ConvertTimeFromUtc(jwtToken.ValidTo, TimeZoneInfo.Local)
+            });
+
+
+}
+    }
+}
